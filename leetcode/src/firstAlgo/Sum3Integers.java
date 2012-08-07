7a5890592622b778798bb3415ced1acb24b98f9a
Xue Wang <xwang51@syr.edu>
2012-08-06 17:38:12 -0700

    a

--- leetcode/src/firstAlgo/Sum3Integers.java
+++ leetcode/src/firstAlgo/Sum3Integers.java
@@ -15,6 +15,33 @@
 
 package firstAlgo;
 
+import java.util.ArrayList;
+
 public class Sum3Integers {
-	aaa
+	public ArrayList<ArrayList<Integer>> threeSum(int[] S) {
+		// Assuming the array has already been sorted in a non-descending order
+		int n = S.length; 
+		ArrayList Triplets = new ArrayList();
+		int[] triplet = new int[3];
+		
+		for (int i = 0; i < n; i++) {
+			int j = i + 1;
+			int k = n - 1;
+			
+			while (j < k) {
+				if (S[i] + S[j] + S[k] < 0) j++;
+				else if (S[i] + S[j] + S[k] < 0) k--;
+				else {
+					triplet[0] = S[i];
+					triplet[1] = S[j];
+					triplet[2] = S[k];
+					Triplets.add(triplet);
+					j++;
+					k--;
+				}
+			}
+		}
+		
+		return Triplets;
+	}
 }
